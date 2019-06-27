f6417431a5ba0b860617ea89e3063fce187f254a
Rodrigo <rcoto@protonmail.com>
2019-06-26 23:34:25 -0700

    Style: make ColorBox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -40,6 +40,18 @@ export default {
       fontSize: "1rem",
       lineHeight: "30px",
       textDecoration: "none"
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "33.3333%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "20%"
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: "10%"
     }
   }
 };
