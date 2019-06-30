3d7b9137cd60282aa3e98340e0b20de19c1fc5b7
Rodrigo <rcoto@protonmail.com>
2019-06-29 22:34:57 -0700

    Refac: PureComponents to imporove preformace

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -26,6 +26,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
 
   openDialog(id) {
@@ -60,7 +61,7 @@ class PaletteList extends Component {
                   openDialog={this.openDialog}
                   key={palette.id}
                   id={palette.id}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  handleClick={this.goToPalette}
                 />
               </CSSTransition>
             ))}
